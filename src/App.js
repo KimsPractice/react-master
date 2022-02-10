ca8a7e8f148f160bd9a233f874d16daafc4c6360
kimspractice <tpghksdldia@naver.com>
2022-02-10 16:59:57 +0900

    extending styled

--- src/App.js
+++ src/App.js
@@ -4,28 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Child1 = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const Child2 = styled.div`
-  background-color: tomato;
+const Child = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.span`
-  color: white;
+const Circle = styled(Child)`
+  border-radius: 50%;
 `;
 
 function App() {
   return (
     <Father>
-      <Child1>
-        <Text>Hello</Text>
-      </Child1>
-      <Child2 />
+      <Child bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
