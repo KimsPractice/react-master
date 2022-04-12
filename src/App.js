6b394f673828fd500b9eb675076bdc119761c4e1
kimspractice <tpghksdldia@naver.com>
2022-04-12 15:00:46 +0900

    Animation and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
-  display: flex;
-`;
-
-const Btn = styled.button`
-  background-color: red;
-  color: white;
-  border-radius: 15px;
-`;
-
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
-`;
-
-function App() {
-  return (
-    <Father>
-      <Btn>Log in</Btn>
-      <Btn as="a" href="">
-        Log in
-      </Btn>
-
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
-  );
+const rotate = keyframes`
+0%{
+  transform: rotate(0);
 }
+50%{
+  transform: rotate(360deg);
+  border-radius: 50%;
+}
+100%{
+  transform: rotate(0);
+}
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotate} 3s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 50px;
+    &:hover {
+      font-size: 100px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
+const App = () => (
+  <Wrapper>
+    <Box>
+      <span>💛</span>
+    </Box>
+  </Wrapper>
+);
 
 export default App;
