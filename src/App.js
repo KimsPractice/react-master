3a0205060892591d4810e289aa5ff61160e75168
kimspractice <tpghksdldia@naver.com>
2022-04-18 13:04:17 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
-const rotate = keyframes`
-0%{
-  transform: rotate(0);
-}
-50%{
-  transform: rotate(360deg);
-  border-radius: 50%;
-}
-100%{
-  transform: rotate(0);
-}
-`;
-
-const Emoji = styled.span`
+const Title = styled.h1`
   font-size: 50px;
+  color: ${(props) => props.theme.fontColor};
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100vw;
   height: 100vh;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotate} 3s linear infinite;
   display: flex;
   justify-content: center;
   align-items: center;
-  ${Emoji} {
-    &:hover {
-      font-size: 100px;
-    }
-  }
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
 const App = () => (
   <Wrapper>
-    <Box>
-      <Emoji as="p">💛</Emoji>
-    </Box>
+    <Title>Hello</Title>
   </Wrapper>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+import { ThemeProvider } from "styled-components";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+const darkTheme = {
+  bgColor: "black",
+  fontColor: "white",
+};
+
+const lightTheme = {
+  bgColor: "white",
+  fontColor: "black",
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
