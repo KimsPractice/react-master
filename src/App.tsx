d8f6428810fe1d450a4bfda275206ec252698458
kimspractice <tpghksdldia@naver.com>
2022-04-20 00:13:43 +0900

    Typing the Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import styled, { keyframes } from "styled-components";
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${(props) => props.theme.fontColor};
-`;
-
-const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.bgColor};
-`;
+import Circle from "./Circle"
 
 const App = () => (
-  <Wrapper>
-    <Title>Hello</Title>
-  </Wrapper>
+  <div>
+    <Circle bgColor="teal"/>
+    <Circle bgColor="tomato"/>
+  </div>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const darkTheme = {
-  bgColor: "black",
-  fontColor: "white",
-};
-
-const lightTheme = {
-  bgColor: "white",
-  fontColor: "black",
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
