1c5da11fd1851b33df32ba05b22e5c68342a5940
kimspractice <tpghksdldia@naver.com>
2022-05-03 16:50:50 +0900

    Styles

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,3 +1,9 @@
-const Coins = () => <h1>Coins</h1>;
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Coins = () => <Title>코인</Title>;
 
 export default Coins;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  textColor: "black",
-  bgColor: "white",
-  btnColor: "tomato",
+  textColor: "#2F3640",
+  bgColor: "#F5F6FA",
+  accentColor: "#44BD32",
 };
