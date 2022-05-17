7df5e269558f6517f9ffaf46ce6e4032d72fd1ed
kimspractice <tpghksdldia@naver.com>
2022-05-17 10:19:13 +0900

    Home part one

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  textColor: "#2F3640",
+  textColor: "#2f3640",
   bgColor: "#F5F6FA",
-  accentColor: "#44BD32",
+  accentColor: "#9c88ff",
 };
